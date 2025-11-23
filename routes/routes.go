8e4f6e8ae1b0aad6bb7ded34d183ba70c3a75ec3
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-24 00:50:05 +0200

    created routes package

---
+++ routes/routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"example.com/event-booking/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(server *gin.Engine){
+	server.GET("/events", controllers.GetAllEvents)
+	server.GET("/events/:id", controllers.GetEventById)
+	server.POST("/events", controllers.CreateEvent)
+}
