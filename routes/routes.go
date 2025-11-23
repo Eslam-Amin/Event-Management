518c757c1755e139f90ea44837f67094f6a545e6
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-24 01:11:21 +0200

    Created update (PUT) event endpoint

--- routes/routes.go
+++ routes/routes.go
@@ -9,4 +9,5 @@ func RegisterRoutes(server *gin.Engine){
 	server.GET("/events", controllers.GetAllEvents)
 	server.GET("/events/:id", controllers.GetEventById)
 	server.POST("/events", controllers.CreateEvent)
+	server.PUT("/events/:id", controllers.UpdateEvent)
 }
