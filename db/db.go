4ab6fda0a6bb5bfd6bae6c03508b6618b40db79b
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-23 17:15:51 +0200

    Created create tables function

--- db/db.go
+++ db/db.go
@@ -17,5 +17,24 @@ func InitDB(){
 	
 	DB.SetMaxIdleConns(10)
 	DB.SetMaxIdleConns(5)
+	createTables()
 }
 
+
+func createTables(){
+	createEventsTable := `CREATE TABLE IF NOT EXISTS events(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		date_time DATETIME NOT NULL,
+		user_id INTEGER NOT NULL,
+		)`
+		
+		
+		_, err :=DB.Exec(createEventsTable) 
+		if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+}
