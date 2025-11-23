dbba2db0ac084f16db1f8a2ad7a6ad9e16622827
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-24 01:42:43 +0200

    Added save to user struct

--- models/user.go
+++ models/user.go
@@ -1,12 +1,36 @@
 package models
 
+import "example.com/event-booking/db"
+
 type User struct {
-	ID int 
+	ID int64
 	Name string `binding:"required"`
 	Email string `binding:"required"`
 	Password string `binding:"required"`
 }
 
+
+func (user *User) Save() error {
+	query := `
+	INSERT TO users
+	(name, email, password)
+	VALUES (?, ?, ?)
+	`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil{
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(user.Name, user.Email, user.Password)
+	if err != nil{
+		return err
+	}
+	userId, err := result.LastInsertId()
+
+	user.ID = userId
+	return err
+}
 
 func NewUser()*User{
 	return &User{}
