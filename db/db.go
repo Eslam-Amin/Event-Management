9565ba9863bd53c9d7136fc75b15c74ca89dca5a
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-24 13:00:46 +0200

    Added uniqeness to the email

--- db/db.go
+++ db/db.go
@@ -32,7 +32,7 @@ func createUsersTable(){
 	CREATE TABLE IF NOT EXISTS users(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL,
-		email TEXT NOT NULL,
+		email TEXT NOT NULL UNIQUE,
 		password TEXT NOT NULL
 	);
 	`
