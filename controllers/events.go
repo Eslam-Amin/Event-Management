a2cdfbc3a1286499fd76d8829364d69f6f22e3ce
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-24 17:28:13 +0200

    Created cancel event registeration controller function

--- controllers/events.go
+++ controllers/events.go
@@ -199,3 +199,36 @@ func RegisterForEvent(context *gin.Context) {
 	})
 
 }
+
+func CancelEventRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Couldn't parse request param.",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusNotFound, gin.H{
+			"message": "Couldn't fetch event!",
+			"error":   err.Error(),
+		})
+		return
+	}
+	err = event.CancelEventRegistration(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Couldn't cancel event registration, try again later!",
+			"error":   err.Error(),
+		})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Event registration cancelled successfully",
+	})
+
+}
